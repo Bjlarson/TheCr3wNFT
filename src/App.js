d9331680f9b3eed09d3ad9cb55984249f88b7f45
Blake <bjlarson2_8@hotmail.com>
2021-10-10 11:43:24 -0600

    Cleaned up unused items and propoed design layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import { ethers } from "ethers";
-import ErrorMessage from "./components/ErrorMessage";
-import TxList from "./components/TxList";
+import './App.css';
+import { Component } from "react";
 
 function MetamaskAccountButton  ()  {
   window.ethereum.request({ method: 'eth_requestAccounts' })
 }
 
-const startPayment = async ({ setError, setTxs, ether, addr }) => {
+const startPayment = async ({ setError, setTxs, ether }) => {
   try {
     if (typeof window.ethereum == 'undefined')
       throw new Error("No crypto wallet found. Please install it.");
 
     await window.ethereum.send("eth_requestAccounts");
+    const addr = "0xD1D976C184B004acBcdA1165276FF0BC16D0f1Dd"
     const provider = new ethers.providers.Web3Provider(window.ethereum);
     const signer = provider.getSigner();
     ethers.utils.getAddress(addr);
@@ -20,7 +21,7 @@ const startPayment = async ({ setError, setTxs, ether, addr }) => {
       to: addr,
       value: ethers.utils.parseEther(ether)
     });
-    console.log({ ether, addr });
+    console.log({ether});
     console.log("tx", tx);
     setTxs([tx]);
   } catch (err) {
@@ -56,79 +57,87 @@ const InteractWithContract = async ({setError, setTxs, Contract_Address}) => {
   }
 };
 
-export default function App() {
-  const [error, setError] = useState();
-  const [txs, setTxs] = useState([]);
+function App(){
+  const accountAddress = window.ethereum.request({ method: 'eth_requestAccounts' });;
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const data = new FormData(e.target);
-    setError();
-    await startPayment({
-      setError,
-      setTxs,
-      ether: data.get("ether"),
-      addr: data.get("addr")
-    });
-    await InteractWithContract({
-        setError,
-        setTxs,
-        Contract_Address: data.get("Contract")
-      });
+  const transactionParameters = {
+    nonce: '0x00', // ignored by MetaMask
+    gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+    gas: '0x2710', // customizable by user during MetaMask confirmation.
+    to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970', // Required except during contract publications.
+    from: accountAddress, // must match user's active address.
+    value: '0x29a2241af62c0000', // Only required to send ether to the recipient from the initiating external account.
+    data:
+      '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+    chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
   };
 
+  const GetMetamaskAccount = () => {
+    console.log(accountAddress[0]);
+  }
+
+  const SendTransaction = () => {
+    console.log(accountAddress)
+     window.ethereum.request({
+      method: 'eth_sendTransaction',
+      params: [transactionParameters],
+    });
+  }
+
+
   return (
-    <div>
-      <form className="m-4" onSubmit={handleSubmit}>
-      <div className="credit-card w-full lg:w-1/2 sm:w-auto shadow-lg mx-auto rounded-xl bg-white">
-        <main className="mt-4 p-4">
-          <h1 className="text-xl font-semibold text-gray-700 text-center">
-            Send ETH payment
-          </h1>
-          <div className="">
-            <div className="my-3">
-              <input
-                type="text"
-                name="addr"
-                className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Recipient Address"
-              />
-            </div>
-            <div className="my-3">
-              <input
-                name="ether"
-                type="text"
-                className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Amount in ETH"
-              />
-              <input
-                name="Contract"
-                type="text"
-                className="input input-bordered block w-full focus:ring focus:outline-none"
-                placeholder="Amount in ETH"
-              />
-            </div>
-          </div>
-        </main>
-        <footer className="p-4">
-          <button
-            type="submit"
-            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
-          >
-            Pay now
-          </button>
-          <ErrorMessage message={error} />
-          <TxList txs={txs} />
-        </footer>
-      </div>
-      </form>
-      <button
-            className="btn btn-primary submit-button focus:ring focus:outline-none w-full"
-            onClick = {InteractWithContract}
-          >
-            Interact With Contract
-          </button>
-          <button class="enableEthereumButton" onClick = {MetamaskAccountButton}>Enable Ethereum</button>
-    </div>
+    <body>
+
+<div className="container"> 
+
+  <header> <a href="">
+    <h4 className="logo">NFT MUSEUM</h4>
+    </a>
+    <nav>
+      <ul>
+        <li><a href="#hero">HOME</a></li>
+		<li><a href="#Museum">MUSEUM</a></li>
+        <li><a href="#about">ABOUT</a></li>
+        <li> <a href="#contact">CONTACT</a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <section className="Featured" id="Featured">
+    <h2 className="hero_header">FEATURED <span class="light">NFT'S</span></h2>
+    <p className="tagline">
+      Check out these NFT's to add to your collection
+    </p>
+    <div class = "FeaturedImages">
+		 <img src="http://cdn.onlinewebfonts.com/svg/img_438684.png" alt="" width="400" height="200" class="Images"/>
+		 <img src="https://www.tynker.com/projects/images/739335ed06c2b01de4fa5360275ad181727d074d/bendy---bendy-copy-copy-copy-1.png" alt="" width="400" height="200" class="Images"/>
+		 <img src="https://i.pinimg.com/originals/fd/94/e1/fd94e14c173efe3f18db004060474edd.png" alt="" width="400" height="200" class="Images"/>
+		 <img src="https://cdn-img-0.wanelo.com/p/422/857/5f1/5d1a094499f43283956ccc1/x354-q80.jpg" alt="" width="400" height="200" class="Images"/>
+		 <img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" class="Images"/>
+		 <img src="https://wpclipart.com/famous/Entertainers/musicians/Elvis/Elvis_Presley_Stencil.png" alt="" width="400" height="200" class="Images"/>
+	  </div>
+  </section>
+  <section className="banner">
+    <h2 className="parallax">Make a Donation</h2>
+    <p className="parallax_description">Help fund our project</p>
+	<div className="Donation">
+		<input className="DonationAmount"  type="text" name="ether"  placeholder="Amount in ETH"/>
+		<button className="DonationButon" type="submit" onClick = {SendTransaction}>Send Donation</button>
+	</div>
+  </section>
+	
+  <section className="footer_banner" id="contact">
+	<div className="Subs">
+    	<p className="button">Insta</p>
+		<p className="button">Git</p>
+	    <p className="button">Email</p>
+	</div>
+    <h2 className="hidden">Footer Banner Section </h2>
+    <p className="hero_header">FOR THE LATEST NEWS &amp; UPDATES</p>
+	</section>
+	</div>
+  </body>
   );
-}
+  }
+
+  export default App;
