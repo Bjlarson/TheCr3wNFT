f683f03871a34c7ce28946182ca44787bfc6f341
Blake <bjlarson2_8@hotmail.com>
2021-10-11 13:55:00 -0600

    got app to be able to send a payment

--- src/App.js
+++ src/App.js
@@ -58,14 +58,13 @@ const InteractWithContract = async ({setError, setTxs, Contract_Address}) => {
 };
 
 function App(){
-  const accountAddress = window.ethereum.request({ method: 'eth_requestAccounts' });;
 
   const transactionParameters = {
     nonce: '0x00', // ignored by MetaMask
     gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
     gas: '0x2710', // customizable by user during MetaMask confirmation.
     to: '0x2f318C334780961FB129D2a6c30D0763d9a5C970', // Required except during contract publications.
-    from: accountAddress, // must match user's active address.
+    from: "", // must match user's active address.
     value: '0x29a2241af62c0000', // Only required to send ether to the recipient from the initiating external account.
     data:
       '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
@@ -73,20 +72,35 @@ function App(){
   };
 
   const GetMetamaskAccount = () => {
-    console.log(accountAddress[0]);
+    console.log("accountAddress");
   }
 
-  const SendTransaction = () => {
-    console.log(accountAddress)
-     window.ethereum.request({
-      method: 'eth_sendTransaction',
-      params: [transactionParameters],
-    });
+  const SendTransaction = async () => {
+    try {
+      if (typeof window.ethereum == 'undefined') 
+        throw new Error("No crypto wallet found. Please install it.");
+
+      const accountAddress = await window.ethereum.request({ method: 'eth_requestAccounts' }).then((res) => {return res;})
+      console.log(accountAddress)
+
+      await window.ethereum.send("eth_requestAccounts");
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = provider.getSigner();
+      ethers.utils.getAddress(accountAddress[0]);
+      const tx = await signer.sendTransaction({
+        to: "0xD1D976C184B004acBcdA1165276FF0BC16D0f1Dd",
+        value: ethers.utils.parseEther(".001")
+      });
+
+      console.log("tx", tx);
+    } catch (err) {
+      console.log(err.message);
+    }
   }
 
 
   return (
-    <body>
+    <div>
 
 <div className="container"> 
 
@@ -104,17 +118,17 @@ function App(){
   </header>
 
   <section className="Featured" id="Featured">
-    <h2 className="hero_header">FEATURED <span class="light">NFT'S</span></h2>
+    <h2 className="hero_header">FEATURED <span className="light">NFT'S</span></h2>
     <p className="tagline">
       Check out these NFT's to add to your collection
     </p>
-    <div class = "FeaturedImages">
-		 <img src="http://cdn.onlinewebfonts.com/svg/img_438684.png" alt="" width="400" height="200" class="Images"/>
-		 <img src="https://www.tynker.com/projects/images/739335ed06c2b01de4fa5360275ad181727d074d/bendy---bendy-copy-copy-copy-1.png" alt="" width="400" height="200" class="Images"/>
-		 <img src="https://i.pinimg.com/originals/fd/94/e1/fd94e14c173efe3f18db004060474edd.png" alt="" width="400" height="200" class="Images"/>
-		 <img src="https://cdn-img-0.wanelo.com/p/422/857/5f1/5d1a094499f43283956ccc1/x354-q80.jpg" alt="" width="400" height="200" class="Images"/>
-		 <img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" class="Images"/>
-		 <img src="https://wpclipart.com/famous/Entertainers/musicians/Elvis/Elvis_Presley_Stencil.png" alt="" width="400" height="200" class="Images"/>
+    <div className = "FeaturedImages">
+		 <img src="https://purepng.com/public/uploads/large/purepng.com-donutdonutdoughnutsweetsnack-1411527416743kegtg.png" alt="" width="400" height="200" className="Images"/>
+		 <img src="https://pngimg.com/uploads/alien/alien_PNG81.png" alt="" width="400" height="200" className="Images"/>
+		 <img src="https://i.pinimg.com/originals/fd/94/e1/fd94e14c173efe3f18db004060474edd.png" alt="" width="400" height="200" className="Images"/>
+		 <img src="https://cdn-img-0.wanelo.com/p/422/857/5f1/5d1a094499f43283956ccc1/x354-q80.jpg" alt="" width="400" height="200" className="Images"/>
+		 <img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" className="Images"/>
+		 <img src="https://pngimg.com/uploads/smoke/smoke_PNG55142.png" alt="" width="400" height="200" class="Images"/>
 	  </div>
   </section>
   <section className="banner">
@@ -129,14 +143,14 @@ function App(){
   <section className="footer_banner" id="contact">
 	<div className="Subs">
     	<p className="button">Insta</p>
-		<p className="button">Git</p>
+		<p className="button">FB</p>
 	    <p className="button">Email</p>
 	</div>
     <h2 className="hidden">Footer Banner Section </h2>
     <p className="hero_header">FOR THE LATEST NEWS &amp; UPDATES</p>
 	</section>
 	</div>
-  </body>
+  </div>
   );
   }
 
