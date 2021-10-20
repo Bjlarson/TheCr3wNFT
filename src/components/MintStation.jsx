ed614b241475cd92adccfaf651d31f4a82149c67
Blake <bjlarson2_8@hotmail.com>
2021-10-19 21:47:13 -0600

    added code for mint station

---
+++ src/components/MintStation.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+function MintStation() {
+  return (
+	<div>
+        <section>
+            <h2 className = "MintHeader">Minting Station</h2>
+            <h4 className = "MintSubHeader">Find the Collection for You</h4>
+        </section>
+        <section>
+            <input className="Search"  type="text" id="SearchPhrase"  placeholder="Search"/>
+            <button className = "SearchButton">Search</button>
+        </section>
+	    <section className="MintItems">
+      		<img src="https://purepng.com/public/uploads/large/purepng.com-donutdonutdoughnutsweetsnack-1411527416743kegtg.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://purepng.com/public/uploads/large/purepng.com-donutdonutdoughnutsweetsnack-1411527416743kegtg.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://pngimg.com/uploads/alien/alien_PNG81.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://i.pinimg.com/originals/fd/94/e1/fd94e14c173efe3f18db004060474edd.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://cdn-img-0.wanelo.com/p/422/857/5f1/5d1a094499f43283956ccc1/x354-q80.jpg" alt="" width="400" height="200" className="Images"/>
+			<img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" className="Images"/>
+			<img src="https://pngimg.com/uploads/smoke/smoke_PNG55142.png" alt="" width="400" height="200" class="Images"/>      			<img src="https://purepng.com/public/uploads/large/purepng.com-donutdonutdoughnutsweetsnack-1411527416743kegtg.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://purepng.com/public/uploads/large/purepng.com-donutdonutdoughnutsweetsnack-1411527416743kegtg.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://pngimg.com/uploads/alien/alien_PNG81.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://i.pinimg.com/originals/fd/94/e1/fd94e14c173efe3f18db004060474edd.png" alt="" width="400" height="200" className="Images"/>
+			<img src="https://cdn-img-0.wanelo.com/p/422/857/5f1/5d1a094499f43283956ccc1/x354-q80.jpg" alt="" width="400" height="200" className="Images"/>
+			<img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" className="Images"/>
+			<img src="https://pngimg.com/uploads/smoke/smoke_PNG55142.png" alt="" width="400" height="200" class="Images"/>		 		
+            <img src="https://i.pinimg.com/originals/49/cd/36/49cd361a200b0a6aa65d564cbe99fced.jpg" alt="" width="400" height="200" className="Images"/>
+			<img src="https://pngimg.com/uploads/smoke/smoke_PNG55142.png" alt="" width="400" height="200" class="Images"/>
+  		</section>
+	</div>
+  );
+}
+
+export default MintStation;
