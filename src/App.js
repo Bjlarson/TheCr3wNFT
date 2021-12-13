a7385058456a3027a0417f0bc9a949f85dabeaa2
Blake <bjlarson2_8@hotmail.com>
2021-12-13 15:40:36 -0700

    updated files to move ethers funtionality

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { ethers } from "ethers";
 import './App.css';
 import Header from "./components/Header";
 import Home from "./components/Home";
@@ -9,78 +8,12 @@ import MarketPlace from "./components/MarketPlace";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import GAabi from "./components/ABI/MuseumGATicket.json";
-
-const ABI = GAabi.abi;
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-
-async function GetTokenId() {
-  const contractAddress = GAabi.networks['3'].address
-  //const contractAddress = '0x1d1583B1c269140575Cc0e8a0Ea125C421bbE6E8';
-
-  //const ABI = ['function GetCurrentTokenId() external view returns(uint)'];
-
-  //const provider = new ethers.providers.getDefaultProvider({'ropsten'});
-  //const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
-  //const signer = provider.getSigner();
-  const museumTicket = new ethers.Contract(contractAddress,ABI,provider);
-  const tokenID = await museumTicket.GetCurrentTokenId();
-  console.log(tokenID.toString())
-}
-
-async function GetRoyaltyFee() {
-  const contractAddress = GAabi.networks['3'].address
-  //const contractAddress = '0x1d1583B1c269140575Cc0e8a0Ea125C421bbE6E8';
-
-  const ABI = ['function GetMintFee() external view returns(uint)'];
-
-  //const provider = new ethers.providers.Web3Provider(window.ethereum);
-  //const provider = new ethers.providers.getDefaultProvider({'ropsten'});
-  const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
-  //const signer = provider.getSigner();
-  const museumTicket = new ethers.Contract(contractAddress,ABI,provider);
-  const feeAmount = await museumTicket.GetMintFee();
-  console.log(feeAmount.toString())
-}
-
-async function GetArtist() {
-  const contractAddress = GAabi.networks['3'].address
-  //const contractAddress = '0x1d1583B1c269140575Cc0e8a0Ea125C421bbE6E8';
-
-  const ABI = ['function GetArtist() external view returns(address)'];
-
-  const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
-  //const provider = new ethers.providers.getDefaultProvider({'ropsten'});
-  //const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
-  const signer = metamaskProvider.getSigner();
-  const museumTicket = new ethers.Contract(contractAddress,ABI,signer);
-  const artist = await museumTicket.GetArtist();
-  console.log(artist)
-}
-
-async function SetMintFee() {
-  const contractAddress = GAabi.networks['3'].address
-  //const contractAddress = '0x1d1583B1c269140575Cc0e8a0Ea125C421bbE6E8';
-
-  const ABI = ['function SetMintFee(uint256 _feeAmount) external'];
-
-  const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
-  //const provider = new ethers.providers.getDefaultProvider({'ropsten'});
-  //const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
-  const signer = metamaskProvider.getSigner();
-  const museumTicket = new ethers.Contract(contractAddress,ABI,signer);
-  museumTicket.SetMintFee(100000000000000);
-}
 
 function App(){
   return (
     <div className="container">
       <Router> 
         <Header/>
-        <button onClick = {() => GetTokenId()}>
-        </button>
-        <button onClick = {() => SetMintFee()}>
-        </button>
         <Switch>
           <Route path="/" exact component={() => <Home />} />
           <Route path="/Museum" exact component={() => <Museum />} />
--- src/components/NFTInfo.jsx
+++ src/components/NFTInfo.jsx
@@ -1,8 +1,42 @@
 import PropTypes from 'prop-types';
 import { ethers } from 'ethers';
+import GAabi from "./components/ABI/MuseumGATicket.json";
+import VIPabi from "./components/ABI/MuseumVIPTicket.json";
+import VIPURI from "./components/URI/VIPTicket.json";
+const contractAddress = VIPabi.networks['3'].address
+async function GetTokenId(contractAddress, ABI) {
+  const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
+  const contract = new ethers.Contract(contractAddress,ABI,provider);
+  const tokenID = await contract.GetCurrentTokenId();
+  return tokenID.toString();
+}
 
-const BuyNFT = (price) => {
-    
+async function GetRoyaltyFee(contractAddress,ABI) {
+  const provider = new ethers.providers.InfuraProvider('ropsten', 'd9d3bb458fde4c72b12ada12da786d99');
+  const contract = new ethers.Contract(contractAddress,ABI,provider);
+  const feeAmount = await contract.GetMintFee();
+  return feeAmount.toString();
+}
+
+async function GetBalance(contractAddress) {
+  const abi = ['function balanceOf(address owner) public view returns (uint256)'];
+  const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
+  const contract = new ethers.Contract(contractAddress,abi,metamaskProvider);
+  const balance = await contract.balanceOf('0xdc24316b9ae028f1497c275eb9192a3ea0f67022');
+  return balance.toString();
+}
+
+const abi = ['function createNFT(address recipient, string memory tokenURI) public payable returns (uint256)']
+async function CreateNFT(contractAddress, ABI ,price) {
+  const metamaskProvider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = metamaskProvider.getSigner();
+  const contract = new ethers.Contract(contractAddress,abi,signer);
+  //const tokenID = await contract.createNFT('0x5B2bFA4735B3209df2c12118Bb971C88292c3d75',VIPURI);
+const tokenID = await signer.sendTransaction({to: contractAddress,
+    value: ethers.utils.formatEther(price),
+    data: contract.createNFT('https://cr3wmuseum.com/VIPTicket.json')}); 
+
+  alert(tokenID.toString())
 }
 
 export default function NFTInfoBox ({title,image,about,price}) {
@@ -12,7 +46,7 @@ export default function NFTInfoBox ({title,image,about,price}) {
             <img src={image} className = "NFTImage" width="300" height="300" ></img>
             <div className = "BoxBottom">
                 <p className = "NFTInfo">{about}</p>
-                <button onClick={() => BuyNFT(price)} className = "BuyNFT">Buy for {price}</button>
+                <button onClick={() => CreateNFT(price)} className = "BuyNFT">Buy for {price}</button>
             </div>
         </div>
     )
