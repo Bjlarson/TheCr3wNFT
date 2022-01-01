fe0d42f68c0ded183af3e2ca043952e11189b801
Blake <bjlarson2_8@hotmail.com>
2022-01-01 08:49:54 -0700

    added a connect to wallet button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link, withRouter } from "react-router-dom";
+import { ethers } from 'ethers';
 
 function Header(props) {
+    const [errorMessage,setErrorMessage] = useState(null);
+    const [defaultAccount, setDefaultAccount] = useState(null);
+    const [connButtonText,setConnButtonText] = useState('Connect Wallet');
+
+    async function ConnectToWalletHandler() {
+        if(window.ethereum){
+            window.ethereum.request({method: 'eth_requestAccounts'})
+            .then(result => {
+                accountChangedhandler(result[0]);
+                setConnButtonText('Wallet Connected');
+            })
+        }else{
+            setErrorMessage('Need to Install Metamask');
+        }
+    }
+
+    function accountChangedhandler(newAccount) {
+        setDefaultAccount(newAccount);
+        alert(newAccount);
+    }
+
   return (
   <header> <Link to="/">
     <h4 className="logo">CR3W MUSEUM</h4>
@@ -46,6 +68,7 @@ function Header(props) {
         </li>
       </ul>
     </nav>
+    <button onClick={() => ConnectToWalletHandler()} className="Walletbutton">{connButtonText}</button>
     </header>
   );
 }
