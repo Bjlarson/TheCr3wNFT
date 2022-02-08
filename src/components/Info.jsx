cce6fb433652c82cbb7c5aadb8d8353d83c8476e
Blake <bjlarson2_8@hotmail.com>
2022-02-08 12:29:55 -0700

    added spotify embeding

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -15,8 +15,9 @@ function Info() {
         <h2 className="AboutHeader">WHAT IS AN NFT?</h2>
         <h4 className="AboutSubHeader">Podcast & Youtube</h4>
       </div>
-      <p className ="AboutParagraph">Listen to our podcast about NFT's on Spotify <a className="URLLink" onClick={() => GoToSite('https://open.spotify.com/episode/56quWx2MDZroSSHw2lTe6F?si=fO_eHTrcQ9KY66TwUXndxw')}>https://open.spotify.com/episode/56quWx2MDZroSSHw2lTe6F?si=fO_eHTrcQ9KY66TwUXndxw</a></p>
-      <p className ="AboutParagraph">Listen to our podcast about NFT's on Ancor.fm <a className="URLLink" onClick={() => GoToSite('https://anchor.fm/thecr3w/episodes/7-What-is-an-NFT---NFTs-Explained-in-LESS-than-20-min-e1dpsme')}>https://anchor.fm/thecr3w/episodes/7-What-is-an-NFT---NFTs-Explained-in-LESS-than-20-min-e1dpsme</a> </p>
+      <iframe src="https://open.spotify.com/embed/episode/56quWx2MDZroSSHw2lTe6F?utm_source=generator" width="100%" height="232" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"></iframe>
+{/*       <p className ="AboutParagraph">Listen to our podcast about NFT's on Spotify <a className="URLLink" onClick={() => GoToSite('https://open.spotify.com/episode/56quWx2MDZroSSHw2lTe6F?si=fO_eHTrcQ9KY66TwUXndxw')}>https://open.spotify.com/episode/56quWx2MDZroSSHw2lTe6F?si=fO_eHTrcQ9KY66TwUXndxw</a></p>
+      <p className ="AboutParagraph">Listen to our podcast about NFT's on Ancor.fm <a className="URLLink" onClick={() => GoToSite('https://anchor.fm/thecr3w/episodes/7-What-is-an-NFT---NFTs-Explained-in-LESS-than-20-min-e1dpsme')}>https://anchor.fm/thecr3w/episodes/7-What-is-an-NFT---NFTs-Explained-in-LESS-than-20-min-e1dpsme</a> </p> */}
       <p className ="AboutParagraph">Learn more on youtube, we recomend this video <a className="URLLink" onClick={() => GoToSite('https://youtu.be/Oz9zw7-_vhM')}>https://youtu.be/Oz9zw7-_vhM</a></p>
     </div>
     <div className="MuseumInfo">
